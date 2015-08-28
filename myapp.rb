4ae142d577bed2b16120da062e9b940fb8257196
Miki <miki.jy12@gmail.com>
2015-08-28 15:15:36 +0900

    中身送信できるようになったよ

--- myapp.rb
+++ myapp.rb
@@ -8,3 +8,20 @@ get '/' do
 	erb :index
 
 end
+
+get'/about' do
+	erb :q
+end
+
+get'/thank' do
+	erb :thank
+end
+
+post '/check' do
+	@email = params[:email]
+	@message = params[:message]
+	erb :check
+end
+# get'/check' do
+# 	erb :check
+# end
