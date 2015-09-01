62e99ea56b23f6b6d7424212b0ac9f8a719c5608
Miki <miki.jy12@gmail.com>
2015-09-01 13:59:04 +0900

    送信ボタンを押してメール送信できるようになった

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,10 @@
+require 'rubygems' # この辺を追記
+require 'bundler'
+Bundler.require
 require 'sinatra'
 require 'sinatra/reloader'
+require 'rest_client'
+
  
 get '/' do
 	# "Hello world!"
@@ -13,15 +18,61 @@ get'/about' do
 	erb :q
 end
 
-get'/thank' do
-	erb :thank
-end
-
 post '/check' do
 	@email = params[:email]
 	@message = params[:message]
 	erb :check
 end
+
+
+ post '/send-mail' do
+ 	@email = params[:email]
+	@message = params[:message]
+	def send_simple_message
+	   RestClient.post "https://api:key-cabe4d08ae4cc3fab6b5946eb54f0123"\
+  "@api.mailgun.net/v3/sandboxcebbc7038ab145f1968fcd1e04eb9142.mailgun.org/messages",
+      :from => "Excited User <mailgun@sandboxcebbc7038ab145f1968fcd1e04eb9142.mailgun.org>",
+	  :to => @email,
+	  :subject => "Hello miki",
+	  :text => @message
+	end
+	send_simple_message
+	erb :thank
+end
+
+
+
+
+# get '/test' do
+# 	send_simple_message
+# 	erb :thank
+# end
+# def send_simple_message
+# 	 RestClient.post "https://api:key-ほにゃらら"\
+# 	 "@api.mailgun.net/v3/sandboxほにゃらら",
+# 	 :from => "Mailgun Sandbox <ほにゃらら>",
+# 	 :to => "miki<ほにゃらら@ほにゃ>",
+# 	 :subject => "Hello miki",
+# 	 :text => "こんにちは"
+# end
+
+
+#  post '/send-mail' do
+#  	@email = params[:email]
+# 	@message = params[:message]
+# 	def send_simple_message
+# 	  RestClient.post "https://api:key-ほにゃらら"\
+# 	  "@api.mailgun.net/v3/sandboxほにゃらら",
+# 	  :from => "Mailgun Sandbox <ほにゃらら>",
+# 	  :to => "miki<ほにゃらら@ほにゃ>",
+# 	  :subject => "Hello miki",
+# 	  :text => "こんにちは"
+# 	end
+# 	send_simple_message
+# 	erb :thank
+# end
+
+
 # get'/check' do
 # 	erb :check
 # end
