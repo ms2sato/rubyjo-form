0d498606cc5fda8abf311f223dbac55d39a0d5da
Miki <miki.jy12@gmail.com>
2015-09-15 01:05:06 +0900

    GitHub整理後

--- myapp.rb
+++ myapp.rb
@@ -1,4 +1,6 @@
-require 'rubygems' # この辺を追記
+# push用
+
+require 'rubygems'
 require 'bundler'
 Bundler.require
 require 'sinatra'
@@ -6,26 +8,13 @@ require 'sinatra/reloader'
 require 'rest_client'
 require 'dotenv'
 Dotenv.load
-require 'yaml'
-require 'active_record'
 
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-
-class Mail < ActiveRecord::Base;
-end
-
-
- 
 get '/' do
-	# "Hello world!"
-	@count = Mail.count
-	@title = "はろーわーるど"
 	erb :index
-
 end
 
-get'/about' do
-	erb :q
+get '/about' do
+	erb :about
 end
 
 post '/check' do
@@ -34,83 +23,17 @@ post '/check' do
 	erb :check
 end
 
-
-# 変数を使ってver.
 post '/send-mail' do
- 	@email = params[:email]
-	@message = params[:message]
-	test = ""
-	test << @email
-	test << "さんからお問い合わせがありました\n内容："
-	test << @message
-	mail = Mail.new
-	mail.email=@email
-	mail.message=@message
-	mail.save
-	send_simple_message(params[:email],params[:message],test)
+	@email = params[:email]
+	@message = params[:message] 
+	send_simple_message
 	erb :thank
 end
-def send_simple_message(email, message,test)
+def send_simple_message
 	RestClient.post "https://api:key-#{ENV['API_KEY']}"\
     "@api.mailgun.net/v3/sandbox#{ENV['SANDBOX_ID']}.mailgun.org/messages",
-    :from => "Excited User <mailgun@sandbox#{ENV['SANDBOX_ID']}.mailgun.org>",
+    :from => "Excited User  <mailgun@sandbox#{ENV['SANDBOX_ID']}.mailgun.org>",
 	:to => "miki<#{ENV['MAIL_TO']}>",
 	:subject => "お問い合わせがありました",
-	:text => test
+	:text => "こんにちは"
 end
-
-
-# 直接打つver.
-# post '/send-mail' do
-#  	@email = params[:email]
-# 	@message = params[:message]
-# 	send_simple_message(params[:email],params[:message])
-# 	erb :thank
-# end
-
-# def send_simple_message(email, message)
-# 	RestClient.post "https://api:key-cabe4d08ae4cc3fab6b5946eb54f0123"\
-#     "@api.mailgun.net/v3/sandboxcebbc7038ab145f1968fcd1e04eb9142.mailgun.org/messages",
-#     :from => "Excited User <mailgun@sandboxcebbc7038ab145f1968fcd1e04eb9142.mailgun.org>",
-# 	:to => "miki<miki.jy12@gmail.com>",
-# 	:subject => "お問い合わせがありました",
-# 	:text => "#{email}さんからお問い合わせがありました。\n内容：#{message}"
-# end
-
-
-
-
-
-# get '/test' do
-# 	send_simple_message
-# 	erb :thank
-# end
-# def send_simple_message
-# 	 RestClient.post "https://api:key-ほにゃらら"\
-# 	 "@api.mailgun.net/v3/sandboxほにゃらら",
-# 	 :from => "Mailgun Sandbox <ほにゃらら>",
-# 	 :to => "miki<ほにゃらら@ほにゃ>",
-# 	 :subject => "Hello miki",
-# 	 :text => "こんにちは"
-# end
-
-
-#  post '/send-mail' do
-#  	@email = params[:email]
-# 	@message = params[:message]
-# 	def send_simple_message
-# 	  RestClient.post "https://api:key-ほにゃらら"\
-# 	  "@api.mailgun.net/v3/sandboxほにゃらら",
-# 	  :from => "Mailgun Sandbox <ほにゃらら>",
-# 	  :to => "miki<ほにゃらら@ほにゃ>",
-# 	  :subject => "Hello miki",
-# 	  :text => "こんにちは"
-# 	end
-# 	send_simple_message
-# 	erb :thank
-# end
-
-
-# get'/check' do
-# 	erb :check
-# end
